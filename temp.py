413e5e09c4877457d261771044779df82adc1699
DanyCanul <31377538+DanyCanul@users.noreply.github.com>
2017-09-04 13:01:33 -0500

    Add files via upload
    Ejemplo

---
+++ temp.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pylab as pl 
+#Vector (arreglo) con los valores del eje x
+x=[1,2,3,4,5]
+#Vector (arreglo) con los valores del eje y
+y=[1,4,9,16,25]
+#Grafica el vector x contra el vector y
+pl.plot(x,y)
+#Guarda la grafica
+pl.savefig('temp1.png') 
