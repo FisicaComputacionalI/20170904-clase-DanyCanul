c2408533c6d61039891059e3dc1eb3b3d4c3f5f3
DanyCanul <31377538+DanyCanul@users.noreply.github.com>
2017-09-04 13:31:41 -0500

    Add files via upload
    Ejercicio terminado

--- temp.py
+++ temp.py
@@ -5,9 +5,9 @@ x=[1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,20
 #Vector (arreglo) con los valores del eje y
 y=[0,0,0,0,0,1,1,1,1,2,2,3,3,4,4,4,5,6,6,7,7]
 #Grafica el vector x contra el vector y
-pl.plot(x,y)
 pl.ylabel('Numero de novios y mascotas')
 pl.xlabel('Anios')
 pl.title('Numero de novios y mascotas por anio de vida')
+pl.plot(x,y, 'm*')
 #Guarda la grafica
 pl.savefig('temp1.png') 
